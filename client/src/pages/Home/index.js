ec6f8902865f2807908e4605c6e72b32ca3182c1
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-01 19:39:13 +0100

    uploading random changes

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+
+
+
+  return (
+   <>
+   <h1 className='home-title'>Zoomies</h1>
+   <div className='btn-grid'>
+<button  className='btn-home'>Start a quizz</button>
+<button className='btn-home'> Leaderboards</button>
+
+   </div>
+   </> 
+  )
+}
+
+export default Home
