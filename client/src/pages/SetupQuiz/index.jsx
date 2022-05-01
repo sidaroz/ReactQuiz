d2b223ab8ec6b82d0e56831e82858b4502adb962
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-01 19:00:32 +0100

    added the range slider for quiz difficulty

--- client/src/pages/SetupQuiz/index.jsx
+++ client/src/pages/SetupQuiz/index.jsx
@@ -7,6 +7,7 @@ function SetupQuiz() {
 
 const [options, setOptions] = useState(null); //refers to the category options fetch
 const [questionCategory, setQuestionCategory] = useState("");
+const [questionDifficulty, setQuestionDifficulty] =  useState("");
 
 //useEffect hook with fetch of the categories from the API endpoint
 useEffect(()=> {
@@ -25,10 +26,18 @@ const handleCategoryChoice = event => {
   setQuestionCategory(event.target.value) 
 }
 
+const handleDifficultyChange = event => {
+  setQuestionDifficulty(event.target.value);
+}
+
 return (
 
 <>
 <main className='quiz-setup-container'>
+
+
+{/* ------Select Category DropDown------ */}
+
 <div>
           <h2>Select Category:</h2>
           <select value={questionCategory} onChange={handleCategoryChoice}>
@@ -44,7 +53,15 @@ return (
           </select>
         </div>
 
-
+{/* ------Select Difficulty Range Slider------ */}
+        
+<div className='range-slider-container'>
+      <div className='slider-wraper'>
+        <input type='range'  min='1' max='3'  className='slider'/>
+          <span className='demo'> 0</span>
+</div>
+</div>
+{/* rembember the value='0' */}
 
 </main>
 
