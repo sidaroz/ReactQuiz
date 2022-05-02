0b0d701d4c1a23e48e72f01a937c1e02c603a2cd
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-02 14:55:47 +0100

    started the layout for quizboard

--- client/src/pages/QuizBoard/Index.jsx
+++ client/src/pages/QuizBoard/Index.jsx
@@ -7,6 +7,23 @@ const QuizBoard = () => {
 
   return (
 <>
+
+<div className='quizboard-container'>
+
+  <div className='question'> This is a question:</div>
+
+  <div>
+    <button>Question1</button>
+    <button>Question2</button>
+    <button>Question3</button>
+    <button>Question4</button>
+  </div>
+
+
+
+</div>
+
+
 </>
 )
 }
