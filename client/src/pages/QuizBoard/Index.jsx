c7a71ea2235ba7d71c9d1bf8188735bdeac6e50b
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-03 02:12:46 +0100

    fixed some mistakes

--- client/src/pages/QuizBoard/Index.jsx
+++ client/src/pages/QuizBoard/Index.jsx
@@ -1,26 +1,11 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
-import axios from 'axios'
 import './style.css'
-import { Axios } from 'axios'
+
 
 const QuizBoard = () => {
 
-  const urlApi = 'https://opentdb.com/api.php?amount=10';
-  const [questions,setQuestions]= useState([]);
-  const [currentIndex,setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    axios.get(urlApi)
-    .then(res => res.data)
-    .then(data => {setQuestions(data.results)})
-   console.log(questions)
-  },[])
-
-
-  return (
+return (
 <>
-
 <div className='quizboard-container'>
 
   <div className='question'> This is a question:</div>
@@ -32,11 +17,7 @@ const QuizBoard = () => {
     <button>Question4</button>
   </div>
 
-
-
 </div>
-
-
 </>
 )
 }
