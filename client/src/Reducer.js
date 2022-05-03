3e2f6bd0a0449fc36488b104d6f98b88b5bf4171
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-03 02:09:52 +0100

    created the reducer

--- client/src/Reducer.js
+++ client/src/Reducer.js
@@ -1,5 +1,6 @@
 const initState = {
     options: {
+    
       loading: false,
       question_category: ``,
       question_difficulty: ``,
@@ -7,4 +8,53 @@ const initState = {
       amount_of_questions: 10
     }
   }
+
+  const Reducer = (state = initState, action) => {
+    switch (action.type) {
+      case "CHANGED_LOADING":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            loading: action.value
+          }
+        }
+      case "CHANGED_CATEGORY":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            question_category: action.value
+          }
+        }
+      case "CHANGED_DIFFICULTY":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            question_difficulty: action.value
+          }
+        }
+      case "CHANGED_TYPE":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            question_type: action.value
+          }
+        }
+      
+      case "CHANGED_NUMBER":
+        return {
+          ...state,
+          options: {
+            ...state.options,
+            amount_of_questions: action.value
+          }
+        }
+      default:
+        return state
+    }
+  }
+  
   export default Reducer
