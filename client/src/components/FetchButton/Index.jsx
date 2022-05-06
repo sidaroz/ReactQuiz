02f95167aced03020cca7a47d12ecea109895c52
sidaroz <sidarozbek96@gmail.com>
2022-05-06 12:00:26 +0100

    comment

--- client/src/components/FetchButton/Index.jsx
+++ client/src/components/FetchButton/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./FetchButton.css";
-
+// This fetches things
 function FetchButton() {
   const questionCategory = useSelector(
     (state) => state.options.question_category
